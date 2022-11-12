19ed90b5b0531cdb50c470f210ddbd726c9ba798
Levente Krizsán <lewalevi@gmail.com>
2022-11-12 16:31:15 +0100

    refactor(project): add correct multi-page config

--- src/mandelbrot/mandelbrot.ts
+++ src/mandelbrot/mandelbrot.ts
@@ -1,12 +1,12 @@
-import { FragmentShader, VertexShader } from "./shader";
-import { ShaderProgram } from "./shader-program";
-import { resizeCanvasToDisplaySize } from "./util";
-import { QuadRenderer } from "./quad-renderer";
+import { FragmentShader, VertexShader } from "../shader";
+import { ShaderProgram } from "../shader-program";
+import { QuadRenderer } from "../quad-renderer";
+import { resizeCanvasToDisplaySize } from "../util";
 
 const canvas = document.querySelector("canvas")!;
 const gl = canvas.getContext("webgl2")!;
 
-const vertexShaderJuliaSource = `#version 300 es
+const vertexShaderMandelbrotSource = `#version 300 es
 precision highp float;
 
 uniform vec2 cameraCenter;
@@ -20,33 +20,31 @@ void main() {
   z0 = position * cameraSize / 2.0 + cameraCenter;
 }`;
 
-const fragmentShaderJuliaSource = `#version 300 es
+const fragmentShaderMandelbrotSource = `#version 300 es
 precision highp float;
 
-uniform vec2 c;
 in vec2 z0;
 out vec4 fragColor;
 
 void main() {
   vec2 z = z0;
   for (int i = 0; i < 100; i++) {
-    z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
+    z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + z0;
   }
   fragColor = (dot(z, z) < 100.0) ? vec4(0, 0, 0, 1) : vec4(1, 1, 1, 1);
 }`;
 
-const vertexShader = new VertexShader(gl, vertexShaderJuliaSource);
-const fragmentShader = new FragmentShader(gl, fragmentShaderJuliaSource);
+const vertexShader = new VertexShader(gl, vertexShaderMandelbrotSource);
+const fragmentShader = new FragmentShader(gl, fragmentShaderMandelbrotSource);
 const program = new ShaderProgram(gl, vertexShader, fragmentShader);
-const juliaRenderer = new QuadRenderer(gl, program);
-const { uniforms } = juliaRenderer;
+const mandelbrotRenderer = new QuadRenderer(gl, program);
+const { uniforms } = mandelbrotRenderer;
 
 resizeCanvasToDisplaySize(gl.canvas);
 
-gl.clearColor(0, 0, 0, 1);
+gl.clearColor(1, 0, 0, 1);
 gl.viewport(0, 0, canvas.width, canvas.height);
 
-let c = [0, 0];
 let cameraCenter = [0, 0];
 let cameraSize = 6;
 
@@ -59,10 +57,6 @@ const pressed = {
   right: false,
   zoomIn: false,
   zoomOut: false,
-  cUp: false,
-  cDown: false,
-  cLeft: false,
-  cRight: false,
 };
 
 function handleKeyDown(e: KeyboardEvent) {
@@ -84,18 +78,6 @@ function handleKeyDown(e: KeyboardEvent) {
       break;
     case "e":
       pressed.zoomIn = true;
-      break;
-    case "ArrowUp":
-      pressed.cUp = true;
-      break;
-    case "ArrowDown":
-      pressed.cDown = true;
-      break;
-    case "ArrowLeft":
-      pressed.cLeft = true;
-      break;
-    case "ArrowRight":
-      pressed.cRight = true;
       break;
     default:
       console.log("Unknown key pressed:", e.key);
@@ -122,29 +104,17 @@ function handleKeyUp(e: KeyboardEvent) {
     case "e":
       pressed.zoomIn = false;
       break;
-    case "ArrowUp":
-      pressed.cUp = false;
-      break;
-    case "ArrowDown":
-      pressed.cDown = false;
-      break;
-    case "ArrowLeft":
-      pressed.cLeft = false;
-      break;
-    case "ArrowRight":
-      pressed.cRight = false;
-      break;
     default:
       console.log("Unknown key pressed:", e.key);
   }
 }
 
-document.addEventListener('keydown', handleKeyDown);
-document.addEventListener('keyup', handleKeyUp);
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
 
 let tPrev = performance.now();
 (function frame() {
-  juliaRenderer.prime();
+  mandelbrotRenderer.prime();
   const t = performance.now();
   const dt = t - tPrev;
   tPrev = t;
@@ -152,7 +122,6 @@ let tPrev = performance.now();
   const zoomVelocity = (pressed.zoomOut ? -v : 0) + (pressed.zoomIn ? +v : 0);
   cameraSize += zoomVelocity * dt;
   const calculatedCameraSize = 6 * Math.exp(-cameraSize + 6);
-  // console.log(cameraSize, calculatedCameraSize);
 
   const lateralVelocity = (pressed.left ? -v : 0) + (pressed.right ? +v : 0);
   const verticalVelocity = (pressed.down ? -v : 0) + (pressed.up ? +v : 0);
@@ -160,15 +129,10 @@ let tPrev = performance.now();
   cameraCenter[0] += lateralVelocity * dt * calculatedCameraSize;
   cameraCenter[1] += verticalVelocity * dt * calculatedCameraSize;
 
-  const cLateralVelocity = (pressed.cLeft ? -v : 0) + (pressed.cRight ? +v : 0);
-  const cVerticalVelocity = (pressed.cDown ? -v : 0) + (pressed.cUp ? +v : 0);
-  c[0] += cLateralVelocity * dt * calculatedCameraSize;
-  c[1] += cVerticalVelocity * dt * calculatedCameraSize;
+  mandelbrotRenderer.setUniform(uniforms.cameraCenter, cameraCenter);
+  mandelbrotRenderer.setUniform(uniforms.cameraSize, calculatedCameraSize);
 
-  juliaRenderer.setUniform(uniforms.c, c);
-  juliaRenderer.setUniform(uniforms.cameraCenter, cameraCenter);
-  juliaRenderer.setUniform(uniforms.cameraSize, calculatedCameraSize);
+  mandelbrotRenderer.draw();
 
-  juliaRenderer.draw();
   requestAnimationFrame(frame);
 })();
